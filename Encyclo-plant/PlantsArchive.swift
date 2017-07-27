4ec9249dbb613b77d0d0b76596d4ce9610b6d8c9
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2017-07-27 20:21:12 +0300

    1st commit Test

---
+++ Encyclo-plant/PlantsArchive.swift
@@ -0,0 +1,19 @@
+//
+//  PlantsArchive.swift
+//  Encyclo-plant
+//
+//  Created by Manolescu Mihai Alexandru on 27/07/2017.
+//  Copyright © 2017 Manolescu Mihai Alexandru. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+func getPlants()->[Plant]
+{
+    let One = Plant()
+    One.itsName = "Camellia sinensis"
+    One.itsImage = #imageLiteral(resourceName: "Csinensis")
+    
+    return [One]
+}
